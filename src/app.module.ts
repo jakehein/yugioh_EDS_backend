4e6ee6c4e35bdc26d67ba63101b4a3f6bd9e343a
jakehein <jacobnolanhein@gmail.com>
2022-09-08 18:21:39 -0700

    added all initial modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { DeckModule } from './deck/deck.module';
+import { CardModule } from './card/card.module';
+import { BoosterPackModule } from './booster-pack/booster-pack.module';
+import { AuthModule } from './auth/auth.module';
+import { SideDeckModule } from './side-deck/side-deck.module';
 
 @Module({
-  imports: [],
+  imports: [
+    UserModule,
+    DeckModule,
+    CardModule,
+    BoosterPackModule,
+    AuthModule,
+    SideDeckModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })

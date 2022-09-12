5fe5f9d6fb67237da223b70209fb825ef14e6d10
jakehein <jacobnolanhein@gmail.com>
2022-09-12 00:17:22 -0700

    revert

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,7 +6,6 @@ import { BoosterPackModule } from './booster-pack/booster-pack.module';
 import { AuthModule } from './auth/auth.module';
 import { SideDeckModule } from './side-deck/side-deck.module';
 import { ConfigModule } from './config/config.module';
-import { EntityManager } from '@mikro-orm/core';
 
 @Module({
   imports: [
